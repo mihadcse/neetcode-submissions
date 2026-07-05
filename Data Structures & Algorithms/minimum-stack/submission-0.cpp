b0e602b6fb4cfdffaf8c908d3521c73c1d46925c
Syed Huzzatullah Mihad <126281163+mihadcse@users.noreply.github.com>
2026-07-05 13:36:27 +0600

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+private:
+    stack<int> st;
+    stack<int>minst;
+public:
+    
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        st.push(val);
+
+        if(minst.empty() || val<=minst.top()){
+            minst.push(val);
+        }
+    }
+    
+    void pop() {
+        if(minst.top() == st.top()){
+            minst.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top();
+    }
+};
